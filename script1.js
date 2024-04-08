783b78fd625ad07f4c3971375da47e3b5061c46b
Ske1sy <131656771+Ske1sy@users.noreply.github.com>
2024-04-08 21:08:53 +0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -2,7 +2,8 @@ function spele() {
     let randomSkaitlis = Math.floor(Math.random() * 100) + 1
     let dati = parseInt(document.getElementById("skaitlis").value)
     if (isNaN(dati) || dati < 1 || dati > 100) {
-        document.getElementById("speles-rezultats").innerHTML = "Ievadiet skaitli no 1 līdz 100.";
+        document.getElementById("speles-rezultats").innerHTML = "Ievadiet skaitli no 1 līdz 100."
+        document.getElementById("speles-rezultats").style.color = "red";
     } else if (dati === randomSkaitlis) {
         document.getElementById("speles-rezultats").innerHTML = "Tu laimēji!";
         document.getElementById("speles-rezultats").style.color = "green"
@@ -13,3 +14,11 @@ function spele() {
 
     document.getElementById("rezultats").innerHTML = randomSkaitlis
 }
+let diena = new Date();
+let nedelasDiena = diena.getDay();
+let dienasNosaukumi = ['Svētdiena', 'Pirmdiena', 'Otrdiena', 'Trešdiena', 'Ceturdiena', 'Piektdiena', 'Sestdiena'];
+let dienasNosaukums = dienasNosaukumi[nedelasDiena];
+document.getElementById("diena").innerHTML = dienasNosaukums
+let datums = new Date();
+let tagadejaisDatums = datums.toLocaleDateString();
+document.getElementById("datums").innerHTML = tagadejaisDatums;
